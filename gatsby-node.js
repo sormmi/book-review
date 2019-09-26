7042bb6e882e07a4dcf5c90922ff863744c056e8
Mika Sormunen <mika.sormunen@citrus.fi>
2019-09-26 23:06:58 +0300

    Prepare to netlify

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,11 @@
-const path = require('path');
+require("dotenv").config({
+  path: ".env",
+})
+const path = require("path")
 
-exports.createPages = ({graphql, actions}) => {
-  const {createPage} = actions;
-  const bookTemplate = path.resolve('src/templates/book-template.js');
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const bookTemplate = path.resolve("src/templates/book-template.js")
 
   return graphql(`
     {
@@ -15,14 +18,14 @@ exports.createPages = ({graphql, actions}) => {
       }
     }
   `).then(result => {
-    if (result.errors) throw result.errors;
+    if (result.errors) throw result.errors
 
-    result.data.allBook.edges.forEach(({node}) => {
+    result.data.allBook.edges.forEach(({ node }) => {
       createPage({
         path: `/book/${node.id}`,
         component: bookTemplate,
-        context: { bookId: node.id }
-      });
-    });
-  });
+        context: { bookId: node.id },
+      })
+    })
+  })
 }
