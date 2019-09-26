5eea2dc12bcd9749a3a20023b092e5b8269d23f8
Mika Sormunen <mika.sormunen@citrus.fi>
2019-09-26 23:43:14 +0300

    Config fix

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA9G4od4PSSiKcm7XCxF39_Nkg-q49BLGw",
+  authDomain: "mso-book-club.firebaseapp.com",
+  databaseURL: "https://mso-book-club.firebaseio.com",
+  projectId: "mso-book-club",
+  storageBucket: "mso-book-club.appspot.com",
+  messagingSenderId: "393285635851",
+  appId: "1:393285635851:web:8ea82ce4fa2536eaf89c0e"
+};
+
+export default firebaseConfig;
