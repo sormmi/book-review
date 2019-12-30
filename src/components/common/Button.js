f809e94282ad4b0c1f1f7c4f079297d06bf3e38d
Mika Sormunen <mika.sormunen@citrus.fi>
2019-12-30 10:37:41 +0200

    Prettier fix 3

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Button = styled.button`
   background: rebeccapurple;
@@ -7,14 +7,14 @@ export const Button = styled.button`
   cursor: pointer;
   border-radius: 4px;
   white-space: nowrap;
-  
-  ${props => props.block ? 'display: block; width: 100%;' : ''}
-  
+
+  ${props => (props.block ? "display: block; width: 100%;" : "")}
+
   &:hover {
     background: indigo;
   }
-  
+
   &:focus {
-    outline:0;
+    outline: 0;
   }
 `;
